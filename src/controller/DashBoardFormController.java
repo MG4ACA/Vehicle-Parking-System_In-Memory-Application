f05f8038ccd9eac701a49744364e9bdeb8fd7b13
MG4ACA <mg4.aca@gmail.com>
2021-08-24 02:05:42 +0530

    1st commit with ui

---
+++ src/controller/DashBoardFormController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashBoardFormController {
+    public ComboBox comboSelectVehicle;
+    public ComboBox comboDriver;
+    public Label lblVehicleType;
+    public Label lblRealTime;
+    public Label lblParkingSlot;
+
+    public void parkVehicleOnAction(ActionEvent actionEvent) {
+    }
+
+    public void onDeliveryShiftOnAction(ActionEvent actionEvent) {
+    }
+
+    public void managementLogInOnAction(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../view/ManagementLoginFrom.fxml"))));
+    }
+
+}
---
+++ src/controller/ManagementLoginFromController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class ManagementLoginFromController {
+    public TextField txtUserName;
+    public TextField txtPassword;
+
+    public void logInOnAction(ActionEvent actionEvent) {
+    }
+
+    public void cancelOnAction(ActionEvent actionEvent) {
+    }
+}
