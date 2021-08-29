b939ae5ff7233c4d02587a9e77fb63610a786101
MG4ACA <mg4.aca@gmail.com>
2021-08-29 19:35:30 +0530

    6th commit some ui modifications

---
+++ src/lk/ijse/parkingSystem/memory/InParkingArray.java
@@ -0,0 +1,41 @@
+package lk.ijse.parkingSystem.memory;
+
+import lk.ijse.parkingSystem.model.InParking;
+
+import java.util.ArrayList;
+
+public class InParkingArray {
+    private static InParkingArray arrays;
+    private ArrayList<InParking> arrayInParking;
+
+    private InParkingArray() {
+
+        arrayInParking=new ArrayList<>();
+
+    }
+
+    public static InParkingArray getInstance() {
+        if (arrays==null){
+            arrays=new InParkingArray();
+        }
+        return arrays;
+    }
+
+
+    public  ArrayList<InParking> getArrayInParking() {
+        return arrayInParking;
+    }
+
+    public boolean setArrayInParking(InParking inParking) {
+        arrayInParking.add(inParking);
+        return true;
+
+    }
+
+    public void removeArraySlots(String vehicleNumber) {
+        boolean b = arrayInParking.removeIf(n -> (n.getVehicleNumber() == vehicleNumber));
+        for (InParking inParking1:arrayInParking) {
+            System.out.println(inParking1);
+        }
+    }
+}
---
+++ src/lk/ijse/parkingSystem/memory/OnDeliveryArray.java
@@ -0,0 +1,43 @@
+package lk.ijse.parkingSystem.memory;
+
+
+import lk.ijse.parkingSystem.model.OnDelivery;
+
+import java.util.ArrayList;
+
+public class OnDeliveryArray {
+    private static OnDeliveryArray arrays;
+    private ArrayList<OnDelivery> arrayOnDelivery;
+
+    private OnDeliveryArray() {
+        arrayOnDelivery =new ArrayList<>();
+
+    }
+
+    public static OnDeliveryArray getInstance() {
+        if (arrays==null){
+            arrays=new OnDeliveryArray();
+        }
+        return arrays;
+    }
+
+
+    public  ArrayList<OnDelivery> getArrayOnDelivery() {
+        return arrayOnDelivery;
+    }
+
+    public boolean setArrayOnDelivery(OnDelivery onDelivery) {
+        arrayOnDelivery.add(onDelivery);
+        return true;
+
+    }
+
+    public void removeArraySlots(String vehicleNumber) {
+        boolean b = arrayOnDelivery.removeIf(n -> (n.getVehicleNumber() == vehicleNumber));
+        System.out.println(vehicleNumber);
+        for (OnDelivery onDelivery :arrayOnDelivery) {
+            System.out.println(b);
+            System.out.println(onDelivery);
+        }
+    }
+}
